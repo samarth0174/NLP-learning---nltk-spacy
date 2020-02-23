5b2d460134dbadb1e7d8eac50c456a684498c361
SAMARTH GUPTA <38130850+samarth0174@users.noreply.github.com>
2020-02-23 20:07:40 +0530

    allupdate

---
+++ task1/task1_pythonfile.py
@@ -0,0 +1,17 @@
+txt = 'Shakespeare gave the world best of theatrics'
+print(txt)
+
+txt = '''After the birth of the twins, Shakespeare left few historical traces until he is mentioned as part of the London theatre scene in 1592. 
+The exception is the appearance of his name in the "complaints bill" of a law case before the Queen's Bench court at Westminster dated Michaelmas Term 1588 and 9 October 1589.
+[32] Scholars refer to the years between 1585 and 1592 as Shakespeare's "lost years".[33] Biographers attempting to account for this period have reported many apocryphal stories. 
+Nicholas Rowe, Shakespeare's first biographer, recounted a Stratford legend that Shakespeare fled the town for London to escape prosecution for deer poaching in the estate of local squire Thomas Lucy. Shakespeare is also supposed to have taken his revenge on Lucy by writing a scurrilous ballad about him.[34][35] Another 18th-century story has Shakespeare starting his theatrical career minding the horses of theatre patrons in London.[36] John Aubrey reported that Shakespeare had been a country schoolmaster.[37] Some 20th-century scholars have suggested that Shakespeare may have been employed as a schoolmaster by Alexander Hoghton of Lancashire, a Catholic landowner who named a certain "William Shakeshafte" in his will.
+[38][39] Little evidence substantiates such stories other than hearsay collected after his death, and Shakeshafte was a common name in the Lancashire area.[40][41]'''
+
+print(txt)
+
+txt = '''सभी कुछ अच्छा चल रहा था। लेकिन एक बार वो बकरी बीमार पड़ी और इस कारण वह धीरे-धीरे कमजोर होने लगी इसलिए अब वो पूरा-पूरा दिन घर पर ही बिताने लेगी। बकरी ने जो खाना पहले से अपने लिए जमा करके रखा था, अब वो भी खत्म होते जा रहा था।
+एक दिन उसकी कुछ बकरी सहेलियां उसका हाल-चाल पूछने उसके पास आईं, तब ये बकरी बड़ी खुश हुई। इसने सोचा कि अपनी सहेलियों से कुछ और दिनों के लिए वह खाना मंगवा लेगी। लेकिन वे बकरियां तो उससे मिलने के लिए अंदर आने से पहले ही उसके घर के बाहर रुक गईं और उसके आंगन में रखा उसका खाना घास-फूस खाने लगीं।
+ये देखकर अब इस बकरी को बहुत बुरा लगा और समझ में आ गया कि उसने अपने जीवन में क्या गलती की? अब वो सोचने लगी कि काश! हर किसी को अपने जीवन का हिस्सा व दोस्त बनाने से पहले उसने उन्हें थोड़ा परख लिया होता है, तो अब इस बीमारी में उसकी मदद के लिए कोई तो होता।'''
+print(txt)
+
+
